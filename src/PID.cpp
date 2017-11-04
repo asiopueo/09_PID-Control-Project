4a34f149e3c1d4acddd29aa4199ebc6226bf6e4a
Martin <martin.lippl@pueo.de>
2017-11-04 17:05:36 +0100

    First light

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -6,16 +7,44 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+	p_error_ = 0.0f;
+	d_error_ = 0.0f;
+	i_error_ = 0.0f;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+
+
+
+void PID::Init(double Kp, double Ki, double Kd) 
+{
+	Kp_ = Kp;
+	Kd_ = Kd;
+	Ki_ = Ki;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte) 
+{
+	d_error_ = cte - p_error_; // diff_cte
+	p_error_ = cte;
+    i_error_ = i_error_ + cte; // int_cte
 }
 
-double PID::TotalError() {
+double PID::TotalError() 
+{
+	double steering;
+	steering = - Kp_ * p_error_ - Kd_ * d_error_ - Ki_ * i_error_;
+
+	cout << "Kp: " << Kp_ << "\t p_error: " << p_error_ << "\t Kd: " 
+		<< Kd_ << "\t d_error: " << d_error_ << "\t Ki: " << Ki_ << "\t i_error: " << i_error_ << endl;
+
+	if (steering >= 1.0f)
+		return 1.0f;
+	else if (steering <= -1.0f)
+		return -1.0f;
+	else
+		return steering;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -1,46 +1,30 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
-public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
+class PID 
+{
+    public:
+        // Errors
+        double p_error_;
+        double i_error_;
+        double d_error_;
 
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
+        // Coefficients
+        double Kp_;
+        double Ki_;
+        double Kd_;
 
-  /*
-  * Constructor
-  */
-  PID();
+        PID();
+        virtual ~PID();
 
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
+        // Initialize PID.
+        void Init(double Kp, double Ki, double Kd);
 
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
+        // Update the PID error variables given cross track error.
+        void UpdateError(double cte);
 
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
-
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+        // Calculate the total PID error.
+        double TotalError();
 };
 
 #endif /* PID_H */
